c9c0cca19867899f7ec461b5febe49e26483fe3b
Kelsi H <kelsimhoyle@gmail.com>
2019-06-06 08:08:39 -0600

    ajax call for youtube api runs on the onclick event of the at home workout. Then a video is displayed on the results div

---
+++ js/youtube-api.js
@@ -0,0 +1,20 @@
+var type = "yoga"
+var subtype;
+var difficulty;
+var queryURL = "https://www.googleapis.com/youtube/v3/search?part=snippet&q=" + type + "&key=AIzaSyADIp92u5gv-wI2vDg05Wwwo6DxAXw4Vs8&type=video&maxResults=5&yvideoEmbeddable=true&videoDuration=medium";
+var title;
+var id;
+
+$("#youtube-api").on("click", function () {
+    $.ajax({
+        url: queryURL,
+        method: "GET"
+    }).then(function (response) {
+        // pick a random number fromt he first 5 responses
+        var firstVideo = response.items[Math.floor(Math.random() * 5) + 1];
+        id = firstVideo.id.videoId;
+        var videoHTML = `<iframe src="https://www.youtube.com/embed/${id}?autoplay=0" width="560" height="315" frameborder="0" allowfullscreen></iframe>`;
+        title = firstVideo.snippet.title;
+        $("#results").html(videoHTML).prepend($("<h5>").text(title));
+    });
+});
