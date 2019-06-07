7ee38f979b4a94633b37b3450ef234d49a214d55
Kelsi H <kelsimhoyle@gmail.com>
2019-06-07 08:00:03 -0600

    adding functionality to the youtube call

--- js/youtube-api.js
+++ js/youtube-api.js
@@ -1,20 +1,22 @@
-var type = "yoga"
-var subtype;
-var difficulty;
-var queryURL = "https://www.googleapis.com/youtube/v3/search?part=snippet&q=" + type + "&key=AIzaSyADIp92u5gv-wI2vDg05Wwwo6DxAXw4Vs8&type=video&maxResults=5&yvideoEmbeddable=true&videoDuration=medium";
-var title;
-var id;
 
 $("#youtube-api").on("click", function () {
+    var type = "yoga";
+    var subtype;
+    var difficulty;
+    var queryURL = "https://www.googleapis.com/youtube/v3/search?part=snippet&q=" + type + "&key=AIzaSyADIp92u5gv-wI2vDg05Wwwo6DxAXw4Vs8&type=video&maxResults=5&yvideoEmbeddable=true&videoDuration=medium";
+    var clickCount = 0;
+
     $.ajax({
         url: queryURL,
         method: "GET"
     }).then(function (response) {
-        // pick a random number fromt he first 5 responses
-        var firstVideo = response.items[Math.floor(Math.random() * 5) + 1];
-        id = firstVideo.id.videoId;
+        var currentVideo = response.items[clickCount];
+        id = currentVideo.id.videoId;
         var videoHTML = `<iframe src="https://www.youtube.com/embed/${id}?autoplay=0" width="560" height="315" frameborder="0" allowfullscreen></iframe>`;
-        title = firstVideo.snippet.title;
-        $("#results").html(videoHTML).prepend($("<h5>").text(title));
+        var title = currentVideo.snippet.title;
+        var id;
+        var newVideo = $("<button>").addClass("btn-large waves-effect waves-light teal lighten-1").text("New Workout");
+        $("#results").html(videoHTML).prepend($("<h5>").text(title)).append(newVideo);
+        
     });
 });
