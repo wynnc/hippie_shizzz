82b703effe9742d89a827a12d598bdebb60991e0
casey is on it <caseyeickhoffdesign@gmail.com>
2019-06-06 21:31:07 -0600

    got the forms to dynamically change based on selection. got the quote of the day api to print the result.

--- js/app.js
+++ js/app.js
@@ -1,16 +1,37 @@
 $(document).ready(function(){
     // var fitnessType = $(".fitness-type").val();
+    $('select').formSelect();
     // console.log("this is fitnessType: " + fitnessType);
     $("select.fitness-type").change(function(){
         var fitnessType = $(this).children("option:selected").val();
+        var subType1 = $(".s12.m4:nth-of-type(1) .input-field:nth-of-type(2) li:nth-of-type(2) span");
+        var subType2 = $(".s12.m4:nth-of-type(1) .input-field:nth-of-type(2) li:nth-of-type(3) span");
+        var subType3 = $(".s12.m4:nth-of-type(1) .input-field:nth-of-type(2) li:nth-of-type(4) span");
         console.log(fitnessType);
-        if(fitnessType === "HIIT"){
-            $(".subFitness option[value='1']").text("CASEY ROCKS!!!");
-            console.log("hellerrr");
+        if(fitnessType === "Bootcamp"){
+            subType1.text("HIIT");
+            subType2.text("Beach Bod");
+            subType3.text("Crossfit");
+        }else if(fitnessType === "Dance"){
+            subType1.text("Hip Hop");
+            subType2.text("Zumba");
+            subType3.text("Belly Dance");
+        }else if(fitnessType === "Yoga"){
+            subType1.text("Hatha");
+            subType2.text("Vinyasa");
+            subType3.text("Restorative");
         }
+    });
+
+    $.ajax({
+        url:"https://favqs.com/api/qotd",
+        method: "GET"
+    }).then(function(response){
+        $("#quote-api").text(response.quote.body);
+        $(".author").text(response.quote.author);
+        console.log(response);
     })
 
-    $('select').formSelect();
 
 
   });
