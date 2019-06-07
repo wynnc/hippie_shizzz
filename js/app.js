b4d1451f767d5eef2cbd6514d854d05e8f45a0ee
casey is on it <caseyeickhoffdesign@gmail.com>
2019-06-06 20:21:36 -0600

    started javascript

--- js/app.js
+++ js/app.js
@@ -1,3 +1,16 @@
 $(document).ready(function(){
+    // var fitnessType = $(".fitness-type").val();
+    // console.log("this is fitnessType: " + fitnessType);
+    $("select.fitness-type").change(function(){
+        var fitnessType = $(this).children("option:selected").val();
+        console.log(fitnessType);
+        if(fitnessType === "HIIT"){
+            $(".subFitness option[value='1']").text("CASEY ROCKS!!!");
+            console.log("hellerrr");
+        }
+    })
+
     $('select').formSelect();
+
+
   });
